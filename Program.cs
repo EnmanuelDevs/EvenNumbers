5ae1c0f188479a2c93a4eef0a5b4f7f05074d192
EnmanuelDevs <enmanueldevs@gmail.com>
2024-10-19 22:32:56 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Even Numbers from 1 to 100:");
+
+            int number = 2;
+
+            while (number <= 100)
+            {
+                Console.WriteLine(number);
+                number += 2;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
